255a6f1c09cfa8a0ec636ec3c9609a9c300cc40c
Arianna <aborgeson@inside.artcenter.edu>
2022-02-17 13:38:23 -0800

    Updated JS

--- assignment4/script.js
+++ assignment4/script.js
@@ -1,10 +1,16 @@
+var expandIMG = document.getElementById("expandedImage");
+var t1 = document.getElementById("thumb-1");
+var t2 = document.getElementById("thumb-2");
+var t3 = document.getElementById("thumb-3");
+var t4 = document.getElementById("thumb-4");
 
-
-function imageExpand(imgs) {
-    var expandIMG = document.getElementById("expandedImage");
-    expandIMG.src = imgs.src;
-    expandIMG.parentElement.style.display = "block";
+function imageExpand(evnt) {
+    console.info(evnt);
+    expandIMG.src = evnt.target.src;
+    // expandIMG.parentElement.style.display = "block";
 }
 
-// expandIMG.addEventListener("click", imageExpand);
-
+t1.addEventListener("click", imageExpand)
+t2.addEventListener("click", imageExpand)
+t3.addEventListener("click", imageExpand)
+t4.addEventListener("click", imageExpand)
